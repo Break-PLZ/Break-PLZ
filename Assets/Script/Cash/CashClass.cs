18df6576e72b10bd47c3932655511dd15f24f7e5
lezin <yhjsoar5@gmail.com>
2021-09-03 21:02:46 +0900

    Add Reading Json in CashScene
    Ooh Yes

---
+++ Assets/Script/Cash/CashClass.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CashClass
+{
+    public string name;
+    public int type;
+    public string chance;
+    public int price;
+    public string img_name;
+
+    // Start is called before the first frame update
+    public void SetProperty(CashClass cash){
+        name = cash.name;
+        type = cash.type;
+        chance = cash.chance;
+        price = cash.price;
+        img_name = cash.img_name;
+    }
+
+    public void InitProperty(){
+        name = "test_name_"+Random.Range(0, 101);
+        type = Random.Range(0,2);
+        chance = "test_chance_"+ Random.Range(0, 101);
+        price = Random.Range(1, 51);
+        img_name = "itemEx2";
+    }
+    
+    public void TestMethod(){
+        Debug.Log("hihi "+img_name);
+    }
+
+    public void Print()
+    {
+        Debug.Log("name = " + name);
+        Debug.Log("type = " + type);
+        Debug.Log("chance = " + chance);
+        Debug.Log("price = " + price);
+        Debug.Log("img_name = " + img_name);
+    }
+
+}
