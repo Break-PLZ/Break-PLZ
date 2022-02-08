5649b80ea23f4972cb4c543b0420e2f16fee9d3c
flamecracker <flamecracker1220@gmail.com>
2022-02-08 13:46:51 +0900

    adjust drag&drop

--- Assets/Script/TeamDeploy/DragWorker.cs
+++ Assets/Script/TeamDeploy/DragWorker.cs
@@ -7,49 +7,58 @@ using UnityEngine.EventSystems;
 public class DragWorker : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     // Start is called before the first frame update
-    public static Vector2 DefaultPos;
-    public static Transform DefaultParent;
     DragAndDropContainer container;
-
-    
+    bool isDragged = false;
     // public static int sibilingIndex;
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        container.gameObject.SetActive(true);
-        container.gameObject.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Image/EmployeeScene/"+this.GetComponent<workerstatus>().worker.img_name);
-        container.worker = this.GetComponent<workerstatus>().worker;
-        // DefaultPos = this.transform.position;
-        // DefaultParent = this.transform.parent;
-        // sibilingIndex = this.transform.GetSiblingIndex();
-        // Debug.Log(sibilingIndex);
-        // this.transform.SetParent(GameObject.FindWithTag("Background").transform);
-        // this.GetComponent<ShowTalents>().OnPointerExit(eventData);
-        // this.GetComponent<ShowTalents>().enabled = false;
+        if(this.gameObject.name.IndexOf("Worker")!=-1){
+            container.gameObject.SetActive(true);
+            container.gameObject.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Image/EmployeeScene/"+this.GetComponent<workerstatus>().worker.img_name);
+            container.worker = this.GetComponent<workerstatus>().worker;
+            isDragged = true;
+        }
+        else{
+            if(this.gameObject.GetComponent<Image>().overrideSprite !=null){
+                container.gameObject.SetActive(true);
+                container.gameObject.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Image/EmployeeScene/"+this.GetComponent<workerstatus>().worker.img_name);
+                container.worker = this.GetComponent<workerstatus>().worker;
+                isDragged = true;
+            }
+            else{
+                return;
+            }
+        }
+        Debug.Log(container.worker.teamNumber);
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-        container.transform.position = eventData.position;
+        if(isDragged){
+            container.transform.position = eventData.position;
+        }
     }
 
     void IEndDragHandler.OnEndDrag(PointerEventData eventData)
     {
-        //container.image.sprite = null;
-        this.GetComponent<workerstatus>().worker.teamNumber = container.worker.teamNumber;
-        container.gameObject.SetActive(false);
-        container.gameObject.GetComponent<Image>().overrideSprite = null;
-        container.worker = new Worker();
-        this.gameObject.SetActive(false);
-        //this.GetComponent<workerstatus>().worker.teamNumber = 
-        // Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // this.transform.position = DefaultPos;
-        // this.transform.SetParent(DefaultParent);
-        // this.transform.SetSiblingIndex(sibilingIndex);
-        // this.GetComponent<ShowTalents>().enabled = true;
+        if(isDragged){
+            if(this.gameObject.name.IndexOf("Worker")!=-1){
+                if(container.worker.teamNumber != 0){
+                    Destroy(this.gameObject);
+                }
+            }
+            else{
+                this.GetComponent<workerstatus>().worker = container.worker;
+                this.gameObject.GetComponent<Image>().overrideSprite = container.GetComponent<Image>().overrideSprite;
+            }
+            container.GetComponent<Image>().overrideSprite = null;
+            container.worker = new Worker();
+            container.gameObject.SetActive(false);
+            isDragged = false;
+        }
     }
     void Start()
     {
-        //workerList = GameObject.Find("WorkerList");
         container = GameObject.Find("TeamDeploy").transform.Find("Container").GetComponent<DragAndDropContainer>();
     }
 
--- Assets/Script/TeamDeploy/DropWorker.cs
+++ Assets/Script/TeamDeploy/DropWorker.cs
@@ -10,9 +10,22 @@ public class DropWorker : MonoBehaviour, IDropHandler
     DragAndDropContainer container;
     void IDropHandler.OnDrop(PointerEventData eventData){
         if(container.gameObject.GetComponent<Image>().overrideSprite!=null){
+            Worker temp = this.GetComponent<workerstatus>().worker;
+            Sprite tempSprite = this.GetComponent<Image>().overrideSprite;
+            int bf = this.GetComponent<workerstatus>().worker.teamNumber;
+            int af = container.worker.teamNumber;
+            
             this.GetComponent<Image>().overrideSprite = container.gameObject.GetComponent<Image>().overrideSprite;
-            container.worker.teamNumber = this.GetComponent<workerstatus>().worker.teamNumber;
-            this.GetComponent<workerstatus>().SetProperty(container.worker);
+            this.GetComponent<workerstatus>().worker = container.worker;
+            
+            container.GetComponent<Image>().overrideSprite = tempSprite;
+            container.worker = temp;
+
+            this.GetComponent<workerstatus>().worker.teamNumber = bf;
+            container.worker.teamNumber = af;
+            if(af==0){
+                container.worker.teamNumber = bf;
+            }
         }
     }
     void Start()
