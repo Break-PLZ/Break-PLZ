3a2f9ce322cae3ce5291585b453f75e0cfb88501
flamecracker <flamecracker1220@gmail.com>
2022-03-11 12:21:50 +0900

    optimization

--- Assets/Script/Employee/SelectWorkers.cs
+++ Assets/Script/Employee/SelectWorkers.cs
@@ -8,22 +8,11 @@ public class SelectWorkers : MonoBehaviour
 {
     public Text text;
     GameObject obj;
-    Managers managers;
+    public Managers managers;
     public int num = 0;
     // Start is called before the first frame update
     void Start()
     {
-        //initialize
-        if(SceneManager.GetActiveScene().name=="EmployeeScene"){
-            managers = GameObject.Find("EmployeeManager").GetComponent<EmployeeManager>();
-        }
-        else if(SceneManager.GetActiveScene().name=="SetMonthScene"){
-            managers = GameObject.Find("SetMonthManager").GetComponent<SetMonthManager>();
-        }
-        else if(SceneManager.GetActiveScene().name=="WorkerManagementScene"){
-            managers = GameObject.Find("WorkerManager").GetComponent<WorkerManager>();
-        }
-        
         obj = text.gameObject;
         if(obj.name.Contains("Server")){
             num = managers.temp.server;
